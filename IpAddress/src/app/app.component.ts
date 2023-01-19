437187c1c8dd98404437e1908461ded235bd1f81
Andersondev429 <anderson-429@hotmail.com>
2023-01-18 23:59:27 -0300

    Pagina criada

--- IpAddress/src/app/app.component.ts
+++ IpAddress/src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import  * as L from 'leaflet';
+import 'mapbox-gl-leaflet';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'IpAddress';
+
+  ngOnInit(): void{
+    var map = L.map('map').setView([51.5, -0.09], 13);
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+  var imgMarcador = L.icon({
+    iconUrl: '../assets/images/icon-location.svg',
+    shadowUrl: 'leaf-shadow.png',
+
+    iconSize:     [35, 45], // size of the icon
+});
+
+  var marker = L.marker([51.5, -0.09], {icon: imgMarcador}).addTo(map);
+  }
 }
